27e7068332c1828db8421d0056c3fd94d033b1d7
divver <guxiaobin@ccssoft.com.cn>
2018-06-07 23:22:42 +0800

    add sleuth+log reporter

--- service2/src/main/java/com/example/service1/TestController.java
+++ service2/src/main/java/com/example/service1/TestController.java
@@ -2,22 +2,34 @@ package com.example.service1;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.ipc.netty.http.server.HttpServerRequest;
-
-import javax.annotation.PostConstruct;
 
 @RestController
 public class TestController {
 	private static Logger logger = LoggerFactory.getLogger(TestController.class);
 
+	@Autowired
+	WebClient webClient;
+
 	@GetMapping(value = "/test3")
 	public Flux<String> test() {
 		logger.info("-------------------->test3");
 		return Flux.just("1", "2", "3");
+	}
+
+	@GetMapping(value = "/test31")
+	public Flux<String> test31() {
+		logger.info("-------------------->test31");
+		return webClient.mutate().baseUrl("http://127.0.0.1:8082/test3")
+				.build().get().retrieve().bodyToFlux(String.class);
 	}
 
 	@GetMapping(value = "/test4")
---
+++ service2/src/main/java/com/example/service1/TraceConfig.java
@@ -0,0 +1,57 @@
+package com.example.service1;
+
+import brave.sampler.Sampler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import zipkin2.Span;
+import zipkin2.reporter.Reporter;
+
+/**
+ * Created by divver on 2018/6/7.
+ */
+@Configuration
+public class TraceConfig {
+
+  @Primary
+  @Bean
+  public Reporter<Span> reporter(){
+    return new LogReporter();
+  }
+
+  @Bean
+  public Sampler defaultSampler() {
+    return Sampler.ALWAYS_SAMPLE;
+  }
+
+  static class LogReporter implements Reporter<Span> {
+    private static final Logger looger = LoggerFactory.getLogger(LogReporter.class);
+
+    @Override
+    public void report(Span span) {
+      looger.info("trace: {}", span);
+    }
+
+  }
+
+//  @Bean
+//  public ZipkinRestTemplateCustomizer myCustomizer(){
+//    return new ZipkinRestTemplateCustomizer(){
+//
+//      @Override
+//      public void customize(RestTemplate restTemplate) {
+//        restTemplate.getInterceptors().add(new ClientHttpRequestInterceptor() {
+//          @Override
+//          public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+//            System.out.print(new String(body));
+//            return null;
+//          }
+//        });
+//      }
+//
+//    };
+//  }
+
+}
